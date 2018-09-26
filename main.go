3933633bd9e4c8262e1d82140b9b112feeac6878
Vladimir Babin <vovababin@gmail.com>
2018-09-26 23:56:42 +0500

    Add postgres settings

--- main.go
+++ main.go
@@ -8,7 +8,46 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/creepypasta-club/creepypasta-backend/models"
+	"github.com/creepypasta-club/creepypasta-backend/roach"
 )
+
+var (
+	db      roach.Roach
+	address string
+)
+
+func init() {
+	address = os.Getenv("CREEPYPASTA_ADDRESS")
+	if address == "" {
+		address = ":9000"
+	}
+	host := os.Getenv("CREEPYPASTA_POSTGRES_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("CREEPYPASTA_POSTGRES_PORT")
+	if port == "" {
+		port = "5432"
+	}
+	user := os.Getenv("CREEPYPASTA_POSTGRES_USER")
+	if user == "" {
+		user = "mycreepypastauser"
+	}
+	password := os.Getenv("CREEPYPASTA_POSTGRES_PASSWORD")
+	if password == "" {
+		password = "mycreepypastapassword"
+	}
+	database := os.Getenv("CREEPYPASTA_POSTGRES_DATABASE")
+	if database == "" {
+		database = "mycreepypastadb"
+	}
+	roach, err := roach.New(roach.Config{Host: host, Port: port, User: user, Password: password, Database: database})
+	if err != nil {
+		log.Panic(err.Error())
+	} else {
+		db = roach
+	}
+}
 
 func TopicsHandler(c *gin.Context) {
 	topics := []models.Topic{
@@ -21,13 +60,9 @@ func TopicsHandler(c *gin.Context) {
 
 func main() {
 	r := gin.Default()
-	address := os.Getenv("CREEPYPASTA_ADDRESS")
-	if address == "" {
-		address = ":9000"
-	}
-
 	v1 := r.Group("/v1")
 	v1.GET("/topics", TopicsHandler)
 
-	log.Fatal(r.Run(address))
+	r.Run(address)
+	db.Close()
 }
